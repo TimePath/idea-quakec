8e8195989c4d0626b4dca4b24ea596c1aaccc6d7
Andrew Hardaker <andrew.hardaker1995@gmail.com>
2014-12-02 11:57:53 +1100

    Implement basic structure view

---
+++ src/com/timepath/quakec/QCStructureViewElement.java
@@ -0,0 +1,133 @@
+package com.timepath.quakec;
+
+import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.ide.util.treeView.smartTree.TreeElement;
+import com.intellij.navigation.ItemPresentation;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.PlatformIcons;
+import com.timepath.quakec.psi.QCFile;
+import com.timepath.quakec.psi.QCFunc;
+import com.timepath.quakec.psi.QCVar;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author TimePath
+ */
+public class QCStructureViewElement implements StructureViewTreeElement {
+    private QCFile element;
+
+    public QCStructureViewElement(QCFile element) {
+        this.element = element;
+    }
+
+    @Override
+    public Object getValue() {
+        return element;
+    }
+
+    @Override
+    public void navigate(boolean requestFocus) {
+        element.navigate(requestFocus);
+    }
+
+    @Override
+    public boolean canNavigate() {
+        return element.canNavigate();
+    }
+
+    @Override
+    public boolean canNavigateToSource() {
+        return element.canNavigateToSource();
+    }
+
+    @NotNull
+    @Override
+    public ItemPresentation getPresentation() {
+        return element.getPresentation();
+    }
+
+    @NotNull
+    @Override
+    public TreeElement[] getChildren() {
+        QCVar[] vars = PsiTreeUtil.getChildrenOfType(element, QCVar.class);
+        QCFunc[] funcs = PsiTreeUtil.getChildrenOfType(element, QCFunc.class);
+        List<TreeElement> treeElements = new LinkedList<TreeElement>();
+        class Child implements StructureViewTreeElement {
+
+            private final PsiElement e;
+            private final Icon i;
+
+            Child(PsiElement element, Icon icon) {
+                this.e = element;
+                this.i = icon;
+            }
+
+            @Override
+            public void navigate(boolean b) {
+
+            }
+
+            @Override
+            public boolean canNavigate() {
+                return false;
+            }
+
+            @Override
+            public boolean canNavigateToSource() {
+                return false;
+            }
+
+            @Override
+            public Object getValue() {
+                return e;
+            }
+
+            @NotNull
+            @Override
+            public ItemPresentation getPresentation() {
+                return new ItemPresentation() {
+                    @Nullable
+                    @Override
+                    public String getPresentableText() {
+                        return e.getText();
+                    }
+
+                    @Nullable
+                    @Override
+                    public String getLocationString() {
+                        return null;
+                    }
+
+                    @Nullable
+                    @Override
+                    public Icon getIcon(boolean b) {
+                        return i;
+                    }
+                };
+            }
+
+            @NotNull
+            @Override
+            public TreeElement[] getChildren() {
+                return EMPTY_ARRAY;
+            }
+        }
+        if (vars != null) {
+            for (final QCVar e : vars) {
+                treeElements.add(new Child(e, PlatformIcons.VARIABLE_ICON));
+            }
+        }
+        if (funcs != null) {
+            for (final QCFunc e : funcs) {
+                treeElements.add(new Child(e, PlatformIcons.FUNCTION_ICON));
+            }
+        }
+        return treeElements.toArray(new TreeElement[treeElements.size()]);
+    }
+}
---
+++ src/com/timepath/quakec/QCStructureViewModel.java
@@ -0,0 +1,30 @@
+package com.timepath.quakec;
+
+import com.intellij.ide.structureView.StructureViewModel;
+import com.intellij.ide.structureView.StructureViewModelBase;
+import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiFile;
+import com.timepath.quakec.psi.QCFile;
+import com.timepath.quakec.psi.QCFunc;
+import com.timepath.quakec.psi.QCVar;
+
+/**
+ * @author TimePath
+ */
+public class QCStructureViewModel extends StructureViewModelBase implements StructureViewModel.ElementInfoProvider {
+    public QCStructureViewModel(PsiFile psiFile, Editor editor) {
+        super(psiFile, editor, new QCStructureViewElement((QCFile) psiFile));
+        this.withSuitableClasses(QCVar.class, QCFunc.class);
+    }
+
+    @Override
+    public boolean isAlwaysShowsPlus(StructureViewTreeElement element) {
+        return false;
+    }
+
+    @Override
+    public boolean isAlwaysLeaf(StructureViewTreeElement element) {
+        return (element instanceof QCFile);
+    }
+}
