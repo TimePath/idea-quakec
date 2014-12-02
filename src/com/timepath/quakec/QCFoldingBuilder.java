d7b1008452683ae8f34504de023c29eae6bf22e2
Andrew Hardaker <andrew.hardaker1995@gmail.com>
2014-12-02 14:35:07 +1100

    Implement braces and folding

---
+++ src/com/timepath/quakec/QCFoldingBuilder.java
@@ -0,0 +1,50 @@
+package com.timepath.quakec;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.folding.FoldingBuilder;
+import com.intellij.lang.folding.FoldingDescriptor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.tree.IElementType;
+import com.timepath.quakec.psi.QCTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author TimePath
+ */
+public class QCFoldingBuilder implements FoldingBuilder {
+    @NotNull
+    @Override
+    public FoldingDescriptor[] buildFoldRegions(@NotNull ASTNode node, @NotNull Document document) {
+        List<FoldingDescriptor> list = new ArrayList<FoldingDescriptor>();
+        buildFolding(node, list);
+        FoldingDescriptor[] descriptors = new FoldingDescriptor[list.size()];
+        return list.toArray(descriptors);
+    }
+
+    private static void buildFolding(ASTNode node, List<FoldingDescriptor> list) {
+        final IElementType elementType = node.getElementType();
+        if (elementType == QCTypes.BLOCK || elementType == QCTypes.BLOCK_SWITCH) {
+            final TextRange range = node.getTextRange();
+            list.add(new FoldingDescriptor(node, range));
+        }
+        for (ASTNode child : node.getChildren(null)) {
+            buildFolding(child, list);
+        }
+    }
+
+    @Nullable
+    @Override
+    public String getPlaceholderText(@NotNull ASTNode node) {
+        return "{...}";
+    }
+
+    @Override
+    public boolean isCollapsedByDefault(@NotNull ASTNode node) {
+        return false;
+    }
+}
